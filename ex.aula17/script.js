a58600dac6ba5b45bb6f0e8a98eb658b30ffa21e
Pedro Cortes <pedrocortesterrao@gmail.com>
2025-08-26 18:39:10 -0300

    js

--- ex.aula17/script.js
+++ ex.aula17/script.js
@@ -1,22 +1,34 @@
-function carregar() {
-    var msg = document.getElementById('msg');
-    var img = document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    msg.innerHTML = `Agora são ${hora} horas.`;
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossí  vel contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-    if(hora >= 0 && hora < 12){
-        // Bom dia!
-        img.src = 'manha.JFIF';
-        document.body.style.background = '#7b9be0';
-    } else if(hora >= 12 && hora < 18){
-        // Boa tarde!
-        img.src = 'tarde.jpg';
-        document.body.style.background = '#d18f15';
-    } else{
-        // Boa noite!
-        img.src = 'noite.JFIF';
-        document.body.style.background = '#515154';
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` // bandeira de chegada
     }
 }
