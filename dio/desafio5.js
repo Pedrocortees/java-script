c082b039405705f53393537394ce44f118b90ac5
Pedro Cortes <pedrocortesterrao@gmail.com>
2025-12-30 12:59:18 -0300

    js desafio dio

---
+++ dio/desafio5.js
@@ -0,0 +1,8 @@
+
+const tab =7;
+
+
+for (let num = 1; num <= 10; num++) {
+    console.log(num * tab);
+     
+}
---
+++ dio/desafio6.js
@@ -0,0 +1,9 @@
+
+const numeros= [1, 23, 45, 6, 8, 34, 12] 
+
+
+for (let i = 0; i < numeros.length; i++) {
+
+  const numero = numeros[i] 
+  if (numero % 2 === 0) {console.log(numero);}
+}
