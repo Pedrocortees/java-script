ba507faa9e2f077c368d5762feb34a12da4145d9
Pedro Cortes <pedrocortesterrao@gmail.com>
2026-02-10 18:17:28 -0300

    att

--- pokedex.js/index.js
+++ pokedex.js/index.js
@@ -1,1 +1,7 @@
-console.log('Sucesso!')
+const offset = 0
+const limit = 10
+const url = 'file:///C:/Users/pedro%20cortes/Documents/html-css/java-script/pokedex.js/index.html'
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.json(jsonBody))
+.catch((error) => console.json(error))
