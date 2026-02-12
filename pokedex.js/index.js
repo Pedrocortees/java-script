f57d6d594e3259ad5cf77e16efd79ef576f6e535
Pedro Cortes <pedrocortesterrao@gmail.com>
2026-02-11 21:01:06 -0300

    att rest pokemons

--- pokedex.js/index.js
+++ pokedex.js/index.js
@@ -1,7 +1,35 @@
-const offset = 0
-const limit = 10
-const url = 'file:///C:/Users/pedro%20cortes/Documents/html-css/java-script/pokedex.js/index.html'
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => console.json(jsonBody))
-.catch((error) => console.json(error))
+const pokemonList = document.getElementById('pokemonList')
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.id}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map(typeDetail => 
+                        `<li class="type">${typeDetail.type.name}</li>`
+                    ).join('')}
+                </ol>
+
+                <img src="${pokemon.sprites.other['dream_world'].front_default}" 
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+pokeApi.getPokemons()
+    .then((pokemons) => {
+        const detailsPromises = pokemons.map(pokemon =>
+            fetch(pokemon.url).then(response => response.json())
+        )
+        return Promise.all(detailsPromises)
+    })
+    .then((pokemonDetails) => {
+        pokemonList.innerHTML = pokemonDetails
+            .map(convertPokemonToLi)
+            .join('')
+    })
+    .catch((error) => console.error(error))
